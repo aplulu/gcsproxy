cb6e5d4cdd3422beb1624042e86c6e25cb3a0852
Aplulu <aplulu.liv@gmail.com>
2023-04-14 07:47:36 +0900

    :+1: Support for responses over 32 MB

--- internal/domain/model/error.go
+++ internal/domain/model/error.go
@@ -3,8 +3,9 @@ package model
 import "errors"
 
 var (
-	ErrInvalidIDToken      = errors.New("invalid id token")
-	ErrInvalidRedirectURL  = errors.New("invalid redirect URL")
-	ErrInvalidState        = errors.New("invalid state")
-	ErrInvalidHostedDomain = errors.New("invalid hosted domain")
+	ErrInvalidIDToken       = errors.New("invalid id token")
+	ErrInvalidRedirectURL   = errors.New("invalid redirect URL")
+	ErrInvalidState         = errors.New("invalid state")
+	ErrInvalidHostedDomain  = errors.New("invalid hosted domain")
+	ErrStreamingUnsupported = errors.New("streaming is unsupported")
 )
--- internal/infrastructure/http/util.go
+++ internal/infrastructure/http/util.go
@@ -1,9 +1,46 @@
 package http
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
+
+	"cloud.google.com/go/storage"
+
+	"github.com/aplulu/gcsproxy/internal/config"
+	"github.com/aplulu/gcsproxy/internal/domain/model"
 )
+
+func writeHeaders(w http.ResponseWriter, attrs *storage.ObjectAttrs, chunked bool) {
+	writeStringHeader(w, "Last-Modified", attrs.Updated.Format(http.TimeFormat))
+	writeStringHeader(w, "Content-Type", attrs.ContentType)
+	writeStringHeader(w, "Content-Disposition", attrs.ContentDisposition)
+	writeStringHeader(w, "Content-Encoding", attrs.ContentEncoding)
+
+	if chunked {
+		writeStringHeader(w, "Transfer-Encoding", "chunked")
+	} else {
+		writeInt64Header(w, "Content-Length", attrs.Size)
+	}
+
+	// do not cache if authentication is enabled
+	if config.AuthType() == "oidc" || config.AuthType() == "basic" {
+		writeStringHeader(w, "Cache-Control", "private, max-age=60")
+	} else {
+		writeStringHeader(w, "Cache-Control", attrs.CacheControl)
+	}
+}
+
+func responseError(w http.ResponseWriter, err error) {
+	switch {
+	case errors.Is(err, storage.ErrObjectNotExist):
+		http.Error(w, "not found", http.StatusNotFound)
+	case errors.Is(err, model.ErrStreamingUnsupported):
+		http.Error(w, "streaming unsupported", http.StatusInternalServerError)
+	default:
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+	}
+}
 
 func writeStringHeader(w http.ResponseWriter, name string, value string) {
 	if len(value) > 0 {
